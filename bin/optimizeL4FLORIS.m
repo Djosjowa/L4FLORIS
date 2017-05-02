e681d4220bcb6ef8c8977be10cef3d0983f6e286
jgovers <j.a.govers@gmail.com>
2017-05-02 16:44:31 +0200

    Combined axial and yaw

--- bin/optimizeL4FLORIS.m
+++ bin/optimizeL4FLORIS.m
@@ -1,13 +1,13 @@
-function[yaw_opt,J_Pws_opt,J_DEL_opt,J_sum_opt] = optimizeL4FLORIS(modelStruct,turbType,siteStruct,optimStruct,DEL_table,Pref, Pbandwidth,plotResults)
+function[a_opt,yaw_opt,J_Pws_opt,J_DEL_opt,J_sum_opt] = optimizeL4FLORIS(modelStruct,turbType,siteStruct,optimStruct,DEL_table,Pref, Pbandwidth,plotResults)
 % Optimization parameters
 optConst   = optimStruct.optConst;
 iterations = optimStruct.iterations;
-input.a    = optimStruct.axInd;
+amin       = optimStruct.minA;
+amax       = optimStruct.maxA;
 yawmin     = optimStruct.minYaw;
 yawmax     = optimStruct.maxYaw;
 N          = size(siteStruct.LocIF,1); % Number of turbines
 DELbaseline = mean(mean(mean(DEL_table.table))); % DEL values are scaled with this value in the cost function
-
 Pref_plot  = zeros(iterations,1)';
 
 % Calculate windspeed distribution in wind-aligned frame
@@ -19,8 +19,10 @@ weightsInflowUncertainty = gaussianWindDistribution(windInflowDistribution,plotR
 % Initialize empty GT-theory matrices
 [J_Pws_opt,J_sum_opt] = deal(-1e10);
 J_DEL_opt             = 1e10;
-yaw                   = zeros(N,1);           
-yaw_opt               = zeros(iterations,N); 
+yaw                   = zeros(N,1);
+yaw_opt               = zeros(iterations,N);
+a                     = .25*ones(N,1);
+a_opt                 = .25*ones(iterations,N); 
 
 % Perform game-theoretic optimization
 disp([datestr(rem(now,1)) ': Starting GT optimization using FLORIS. [Iterations: ' num2str(iterations) '. Calls to FLORIS: ' num2str(iterations*length(windInflowDistribution)) ']']); tic;
@@ -31,22 +33,31 @@ for k = 1:iterations  % k is the number of iterations
     if k > 1
         for i = 1:N                 % For each WT
             R1 = rand();            % Random value between [0 1]
+            R2 = rand();
             E = 1-k/iterations;     % Sensitivity linearly related to iteration
             if R1 < E
-                R2 = normrnd(0,35); % Perturb with random value [0 10] degrees
-                yaw(i) = max(min(yaw_opt(i)+R2,yawmax),yawmin);
+                R3 = normrnd(0,0.2); % Perturb with random value
+                a(i) = max(min(a_opt(i)+R3,amax),amin);
             else
-                yaw(i) = yaw_opt(k-1,i);   % Keep old yaw setting [degrees]
-            end;
-        end;
-    end;
+                a(i) = a_opt(k-1,i);  
+                if R2 < E
+                    R4 = normrnd(0,35);
+                    yaw(i) = max(min(yaw_opt(i)+R4,yawmax),yawmin);
+                else
+                    yaw(i) = yaw_opt(k-1,i);
+                end
+            end
+        end
+    end
     
     for jj = 1:length(windInflowDistribution) % Calculate power and DEL for each wind direction
         windDir = windInflowDistribution(jj);
         siteStruct.uInfIf = windSpeed*cosd(windDir);
         siteStruct.vInfIf = windSpeed*sind(windDir);
         
-        input.yaw  = yaw;
+        input.a   = a;
+        input.yaw = yaw;
+        
         [turbines, wakes, wtRows] = run_floris(input,modelStruct,turbType,siteStruct);
         
         [P,DEL]  = deal(zeros(1,N));
@@ -62,14 +73,14 @@ for k = 1:iterations  % k is the number of iterations
                 DEL(turbi) = 0; 
             end
             % DEL(turbi) = 1; % Placeholder
-        end;
+        end
         
         Ptot   = sum(P);
         DELtot = sum(DEL);
         
         Ptot_inflows(1,jj)   = Ptot;   % Store results for each wind direction
         DELtot_inflows(1,jj) = DELtot; % Store results for each wind direction
-    end;
+    end
     
     % Calculate collective results over entire wind rose
     sum_Ptot    = Ptot_inflows   * weightsInflowUncertainty;  % Inflow uncertainty-weighed generated power
@@ -77,30 +88,45 @@ for k = 1:iterations  % k is the number of iterations
     sum_PDELtot = 1-optConst*((Pref-sum_Ptot)/Pbandwidth)^2 - (1-optConst)*sum_DELtot/DELbaseline; % Generate combined power and loads cost function
     
     if (sum_PDELtot >= J_sum_opt | k == 1)
-        yaw_opt(k,:) = yaw;
-        J_Pws_opt(k) = sum_Ptot;
-        J_DEL_opt(k) = sum_DELtot;
-        J_sum_opt(k) = sum_PDELtot;
-        J_sum_sub_DEL(k) = sum_DELtot/DELbaseline;
-        J_sum_sub_P(k) = ((Pref-sum_Ptot)/Pbandwidth)^2;
+        a_opt(k,:)       = a;
+        yaw_opt(k,:)     = yaw;
+        J_Pws_opt(k)     = sum_Ptot;
+        J_DEL_opt(k)     = sum_DELtot;
+        J_sum_opt(k)     = sum_PDELtot;
+        J_sum_sub_DEL(k) = (1-optConst)*sum_DELtot/DELbaseline;
+        J_sum_sub_P(k)   = optConst*((Pref-sum_Ptot)/Pbandwidth)^2;
     else % if no improvements: keep optimal solution
-        yaw_opt(k,:) = yaw_opt(k-1,:);
-        J_Pws_opt(k) = J_Pws_opt(k-1);
-        J_DEL_opt(k) = J_DEL_opt(k-1);
-        J_sum_opt(k) = J_sum_opt(k-1);
+        a_opt(k,:)       = a_opt(k-1,:);
+        yaw_opt(k,:)     = yaw_opt(k-1,:);
+        J_Pws_opt(k)     = J_Pws_opt(k-1);
+        J_DEL_opt(k)     = J_DEL_opt(k-1);
+        J_sum_opt(k)     = J_sum_opt(k-1);
         J_sum_sub_DEL(k) = J_sum_sub_DEL(k-1);
-        J_sum_sub_P(k) = J_sum_sub_P(k-1);
-    end;
+        J_sum_sub_P(k)   = J_sum_sub_P(k-1);
+    end
     
     Pref_plot(k) = Pref;
-end;
+    
+    if k > 0.05*iterations
+        J_sum_opt_95(k)     = J_sum_opt(k);
+        J_sum_sub_DEL_95(k) = J_sum_sub_DEL(k);
+        J_sum_sub_P_95(k)   = J_sum_sub_P(k);
+    else
+        J_sum_opt_95(k)     = NaN;
+        J_sum_sub_DEL_95(k) = NaN;
+        J_sum_sub_P_95(k)   = NaN;
+    end
+    
+end
 disp([datestr(rem(now,1)) ': Elapsed time is ' num2str(toc) ' seconds.']);
 
 %% Plotting results
 if plotResults
     disp([datestr(rem(now,1)) ': Plotting results...'])
-    figure % Cost function
-    subplot(3,1,1);
+    
+    figure 
+    % Cost function
+    fig1 = subplot(4,1,1);
     hold on;
     plot(J_sum_opt,'Linewidth',2); grid on;
     plot(J_sum_sub_DEL);
@@ -109,8 +135,17 @@ if plotResults
     ylabel('PL-score [-]'); xlabel('Iterations [-]');
     legend('PL-score','DEL part','Power part');
     
+    fig2 = subplot(4,1,2);
+    hold on
+    plot(J_sum_opt_95,'Linewidth',2); grid on;
+    plot(J_sum_sub_DEL_95);
+    plot(J_sum_sub_P_95);
+    title('Mixed optimization: Power & Load, last 95% of iterations');
+    ylabel('PL-score [-]'); xlabel('Iterations [-]');
+    legend('PL-score','DEL part','Power part');
+    
     % Summed generated power
-    subplot(3,1,2);
+    fig3 = subplot(4,1,3);
     hold on
     plot(J_Pws_opt/1E6,'Linewidth',2);
     plot(Pref_plot/1E6,'--','Linewidth',1);
@@ -118,10 +153,12 @@ if plotResults
     ylabel('Power [MW]'); xlabel('Iterations [-]');
     
     % Summed DEL values
-    subplot(3,1,3);
+    fig4 = subplot(4,1,4);
     plot(J_DEL_opt/1E6,'Linewidth',2)
     grid on; title('Summed DEL values')
     ylabel('DEL (10^6)'); xlabel('Iterations [-]')
+    
+    linkaxes([fig1 fig2 fig3 fig4],'x')
     
     % evaluate nominal wind direction, optimal settings, and plot output
     siteStruct.uInfIf      = windSpeed*cosd(windDirection);
@@ -130,5 +167,5 @@ if plotResults
     plots.plot2DFlow = true  ; % 2DflowFieldvisualisation in wind-aligned frame
     plots.plot3DFlow = false ; % 3DflowFieldvisualisation in wind-aligned frametimer.script = tic;
     run_floris(input,modelStruct,turbType,siteStruct,plots);
-end;
+end
 end
