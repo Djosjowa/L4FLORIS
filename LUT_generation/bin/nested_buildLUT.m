97840e01fe2c57a585935cbfec2ab9559ceb1ad4
Jochem Hoorneman <j.d.hoorneman@student.tudelft.nl>
2017-05-25 12:50:20 +0200

    An almost complete 4D LUT. And buildLUT has an the option to overwrite existing DEL values

--- LUT_generation/bin/nested_buildLUT.m
+++ LUT_generation/bin/nested_buildLUT.m
@@ -1,4 +1,4 @@
-function [saveMat,saveMatIdx,DELsAdded] = nested_buildLUT( inputData,saveMat,saveMatIdx,paramsLoop,rangeLoop,DELsAdded,Ld )
+function [saveMat,saveMatIdx,counter] = nested_buildLUT( inputData,saveMat,saveMatIdx,paramsLoop,rangeLoop,counter,Ld )
 if nargin <= 5 % Set up initial settings
     % Setup inputData parameters
     paramsLoop = inputData.parameters;
@@ -18,7 +18,8 @@ if nargin <= 5 % Set up initial settings
         saveMat = zeros(Nd);
     end
     Ld = 1;
-    DELsAdded = 0;
+    counter.DELsAdded = 0;
+    counter.DELsOverwritten = 0;
 end
 %Nested loop for N-dimensional LUT table building
 % Ld = loop depth
@@ -26,15 +27,21 @@ if length(paramsLoop) >= 1
     for i = 1:length(rangeLoop{1})
         saveMatIdx{Ld} = i;
         inputData.(paramsLoop{1}) = rangeLoop{1}(i); % Update corresponding parameter
-        [saveMat,saveMatIdx,DELsAdded] = nested_buildLUT(inputData,saveMat,saveMatIdx,{paramsLoop{2:end}},{rangeLoop{2:end}},DELsAdded,Ld+1);
+        [saveMat,saveMatIdx,counter] = nested_buildLUT(inputData,saveMat,saveMatIdx,{paramsLoop{2:end}},{rangeLoop{2:end}},counter,Ld+1);
         
         % Read data from txt files
         if length(paramsLoop) == 1  % Save data at lowest level
             DELfile = [cd '\DEL_files\' inputData.DELSetName '\' nested_filenamer(inputData) '.txt'];
             if exist(DELfile,'file')
                 DEL = dlmread(DELfile);
-                saveMat(saveMatIdx{:})  = DEL; % Save file names corresponding to entries
-                DELsAdded = DELsAdded+1;
+                if saveMat(saveMatIdx{:}) == 0
+                    saveMat(saveMatIdx{:})  = DEL; % Save file names corresponding to entries
+                    counter.DELsAdded = counter.DELsAdded+1;
+                elseif inputData.doOverwrite
+                    saveMat(saveMatIdx{:})  = DEL; % Save file names corresponding to entries
+                    counter.DELsOverwritten = counter.DELsOverwritten+1;
+                end
+                
             end
         end
     end
