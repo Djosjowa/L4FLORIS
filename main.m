ba678ecd1fa3a6e15b4e202ebe86b90cadcef71a
jgovers <j.a.govers@gmail.com>
2017-05-30 15:40:46 +0200

    Small change to initial values

--- main.m
+++ main.m
@@ -32,13 +32,13 @@ siteStruct.rho      = 1.1716;   % Atmospheric air density (kg/m3)
 
 % Setup optimization settings
 optimStruct.optConst        = 0.5;                          % Weighting factor. Power only = 1, Loads only = 0.
-optimStruct.iterations      = 100;                            % Optimization iterations  [-]
+optimStruct.iterations      = 1000;                            % Optimization iterations  [-]
 optimStruct.minYaw          = -30;                          % Smallest yaw angle [radians]
 optimStruct.maxYaw          = +30;                          % Largest  yaw angle [radians]
 optimStruct.initYaw         = 0;                            % Yaw angle used for first iteration [radians]
 optimStruct.minA            = 0;                            % Smallest axial induction factor
-optimStruct.maxA            = 0.31;                         % Largest axial induction factor
-optimStruct.initA           = 1/3;                          % Axial induction factor used for first iteration
+optimStruct.maxA            = 1/3;                         % Largest axial induction factor
+optimStruct.initA           = 0.08;                          % Axial induction factor used for first iteration
 optimStruct.windUncertainty = [-12:4:12];                   % Additional wind disturbance range (symmetric)
 
 
